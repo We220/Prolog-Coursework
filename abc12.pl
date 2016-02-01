ee8b52523eab5f2c87720ea0987ad0c75bd29e38
We220 <we220@bath.ac.uk>
2016-02-01 17:10:27 +0000

    Added specification details

---
+++ abc12.pl
@@ -0,0 +1,38 @@
+/* <username and name of the author>
+
+THIS WORK IS ENTIRELY MY OWN.
+
+The program does <or does not, choose which is appropriate> produce multiple answers.
+
+I have <or I have not, choose which is appropriate> used built-ins.
+
+1. <Number of elements in the list binding Q after executing s1(Q,100)>
+<At most 300 characters of clear text on the main idea for the definition of s1.>
+
+2. <Number of elements in the list binding Q after executing s2(Q,100)>
+<At most 300 characters of clear text on the main idea for the definition of s2.>
+
+3. <Number of elements in the list binding Q after executing s3(Q,100)>
+<At most 300 characters of clear text on the main idea for the definition of s3.>
+
+4.
+<At most 300 characters of clear text on the main idea for the definition of s4.>
+
+5. <Number of elements in the list binding Q after executing s4(Q,500)>
+s4(Q,500) uses <number> inferences. */
+
+
+/* <BODY OF THE PROGRAM> */
+
+
+/*
+?- consult(<username>).
+% <username> compiled 0.00 sec, <...> bytes
+true.
+?- time(s1(Q,100)).
+% <...> inferences, <...> CPU in <...> seconds (100% CPU, <...> Lips)
+Q = [[3,4,7,12],[2,6,8,12], <rest of complete list of quadruples>]
+?- time(s2(Q,100)).
+Q=<...>
+<...>
+*/
