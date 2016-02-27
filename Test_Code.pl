6e79933f1069edb01b1131ba9217cc35041e809c
We220 <we220@bath.ac.uk>
2016-02-27 22:37:27 +0000

    Generated list now need to sort it by product

--- Test_Code.pl
+++ Test_Code.pl
@@ -4,7 +4,26 @@
 
 
 
-g_List(98,99,A,[[98,99,197,9702]|A]). % End case for the list
-g_List(X,99,L,A) :- Z is X + 1, Y is Z + 1, S is X+99, P is X*99, g_List(Z,Y,[[X,99,S,P]|L],A), % Increments X and resets Y
-					!.
-g_List(X,Y,L,A) :- Z is Y + 1, S is X+Y, P is X*Y, g_List(X,Z,[[X,Y,S,P]|L],A). % Increments Y
+g_List(2,3,A,[[2,3,5,6]|A]). 
+g_List(2,Y,L,A) :- Z is Y - 1, X is Z - 1, S is Y+2, P is Y*2, g_List(X,Z,[[2,Y,S,P]|L],A), 
+                   !.
+g_List(X,Y,L,A) :- X+Y > 100, Z is X - 1 , g_List(Z,Y,L,A),
+                   !.
+g_List(X,Y,L,A) :- Z is X - 1, S is X+Y, P is X*Y, g_List(Z,Y,[[X,Y,S,P]|L],A).
+
+
+%g_List(,,A,[[,,100,196]|A]). % End case for the list
+%g_List(X,,L,A) :- Z is X + 1, Y is Z + 1, S is X+99, P is X*99, g_List(Z,Y,[[X,99,S,P]|L],A), % Increments X and resets Y
+%					!.
+%g_List(X,Y,L,A) :- X+Y > 100, Z is Y-100, g_List(X,Z,L,A).
+%g_List(X,Y,L,A) :- Z is Y + 1, S is X+Y, P is X*Y, g_List(X,Z,[[X,Y,S,P]|L],A). % Increments Y 
+
+
+%g_List(98,99,A,[[98,99,197,9702]|A]). % End case for the list
+%g_List(X,99,L,A) :- Z is X + 1, Y is Z + 1, S is X+99, P is X*99, g_List(Z,Y,[[X,99,S,P]|L],A), % Increments X and resets Y
+%					!.
+%g_List(X,Y,L,A) :- Z is Y + 1, S is X+Y, P is X*Y, g_List(X,Z,[[X,Y,S,P]|L],A). % Increments Y 
+
+
+
+
